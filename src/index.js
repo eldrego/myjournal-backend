95459501ad4b0c749162d1217bf736759147ef64
Mark Edomwande <medomwande@gmail.com>
2019-05-31 09:06:55 +0100

    modify index to deploy

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import http from 'http';
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import winston from 'winston';
+// import winston from 'winston';
 
 import database from './config/database';
 import routes from './routes';
 
 require('dotenv').config();
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.server = http.createServer(app);
@@ -25,19 +25,11 @@ app.use(cors({
 app.use(bodyParser.json({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(`${__dirname}/../client/dist`));
-app.use(express.static(`${__dirname}/../client/src/assets`));
-app.use(express.static(`${__dirname}/./public`));
-
 app.get('/', (req, res) => {
   res.send({ message: 'Root - Access endpoints using /api/v1' });
 });
 
 app.use('/api/v1/', routes);
 
-if (!module.parent) {
-  app.server.listen(port);
-  winston.info(`Started on port ${port}`, 'info');
-}
-
+app.server.listen(port);
 module.exports = app;
